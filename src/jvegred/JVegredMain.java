36b2413246e577afd026b4093bb3f4e5c95f41b5
pgvalle <pgvalle.nogueira@gmail.com>
2021-03-25 08:42:39 -0300

    Name of main class changed suffix to Main

---
+++ src/jvegred/JVegredMain.java
@@ -0,0 +1,14 @@
+package jvegred;
+
+public class JVegredMain {
+
+    /**
+     * Entry point for app
+     * 
+     * @param args list of commandline arguments
+     */
+    public static void main(String[] args) {
+        JVegredFrame frame = new JVegredFrame("bruh", 500, 500);
+        frame.setVisible(true);
+    }
+}

6224d438941a051149162ff511ff9dd41c560eb5
pgvalle <pgvalle.nogueira@gmail.com>
2021-03-29 21:42:33 -0300

    Added experimental keyboard input features

--- src/jvegred/JVegredFrame.java
+++ src/jvegred/JVegredFrame.java
@@ -2,12 +2,8 @@ package jvegred;
 
 import java.awt.*;
 import java.awt.event.*;
-import java.util.Random;
-import java.util.Vector;
 
 import javax.swing.*;
-import javax.swing.text.JTextComponent.KeyBinding;
-import javax.xml.crypto.dsig.keyinfo.KeyInfo;
 
 import jvegred.figures.Figure;
 import jvegred.figures.RectangleF;
@@ -15,32 +11,48 @@ import jvegred.figures.RectangleF;
 public class JVegredFrame extends JFrame {
 
     private static final long serialVersionUID = 1L;
-    private RectangleF fig;
-    private boolean increase;
+    private Figure fig;
+    private boolean onResize;
+    private boolean onRotate;
+    private boolean onMoving;
 
     JVegredFrame(String title, int w, int h) {
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         this.addKeyListener(new KeyAdapter() {
             public void keyPressed(KeyEvent event) {
-                if (increase) {
-                    if (event.getKeyCode() == KeyEvent.VK_UP) {
-                        fig.resize(0, -2, 0, 0);
-                    } else if (event.getKeyCode() == KeyEvent.VK_DOWN) {
-                        fig.resize(0, 0, 0, 2);
-                    } else if (event.getKeyCode() == KeyEvent.VK_LEFT) {
-                        fig.resize(-2, 0, 0, 0);
+                if (onRotate) {
+                    if (event.getKeyCode() == KeyEvent.VK_LEFT) {
+                        fig.rotate(10);
                     } else if (event.getKeyCode() == KeyEvent.VK_RIGHT) {
-                        fig.resize(0, 0, 2, 0);
+                        fig.rotate(-10);
                     }
-                } else {
+                } else if (onResize) {
                     if (event.getKeyCode() == KeyEvent.VK_UP) {
-                        fig.resize(0, 0, 0, -2);
+                        fig.resize(0, -10, 0, 0);
                     } else if (event.getKeyCode() == KeyEvent.VK_DOWN) {
-                        fig.resize(0, 2, 0, 0);
+                        fig.resize(0, 0, 0, 10);
                     } else if (event.getKeyCode() == KeyEvent.VK_LEFT) {
-                        fig.resize(0, 0, -2, 0);
+                        fig.resize(-10, 0, 0, 0);
                     } else if (event.getKeyCode() == KeyEvent.VK_RIGHT) {
-                        fig.resize(2, 0, 0, 0);
+                        fig.resize(0, 0, 10, 0);
+                    }else if (event.getKeyCode() == KeyEvent.VK_W) {
+                        fig.resize(0, 0, 0, -10);
+                    } else if (event.getKeyCode() == KeyEvent.VK_S) {
+                        fig.resize(0, 10, 0, 0);
+                    } else if (event.getKeyCode() == KeyEvent.VK_A) {
+                        fig.resize(0, 0, -10, 0);
+                    } else if (event.getKeyCode() == KeyEvent.VK_D) {
+                        fig.resize(10, 0, 0, 0);
+                    }
+                } else if (onMoving) {
+                    if (event.getKeyCode() == KeyEvent.VK_UP) {
+                        fig.move(0, -10);
+                    } else if (event.getKeyCode() == KeyEvent.VK_DOWN) {
+                        fig.move(0, 10);
+                    } else if (event.getKeyCode() == KeyEvent.VK_LEFT) {
+                        fig.move(-10, 0);
+                    } else if (event.getKeyCode() == KeyEvent.VK_RIGHT) {
+                        fig.move(10, 0);
                     }
                 }
                 repaint();
@@ -50,16 +62,19 @@ public class JVegredFrame extends JFrame {
         this.addKeyListener(new KeyAdapter() {
             public void keyPressed(KeyEvent event) {
                 if (event.getKeyCode() == KeyEvent.VK_I) {
-                    increase = true;
-                } else if (event.getKeyCode() == KeyEvent.VK_J) {
-                    increase = false;
+                    onResize = true; onRotate = false; onMoving = false;
+                } else if (event.getKeyCode() == KeyEvent.VK_M) {
+                    onMoving = true; onRotate = false; onResize = false;
+                } else if (event.getKeyCode() == KeyEvent.VK_R) {
+                    onRotate = true; onResize = false; onMoving = false;
                 }
             }
         });
 
-        this.fig = new RectangleF(60, 90, 0.0f, Color.WHITE, Color.BLACK, 0);
+        this.fig = new RectangleF(60, 90, 0.0f, Color.WHITE, Color.BLACK, 2);
         this.fig.resize(0, 0, 60, 60);
-        this.increase = true;
+        this.onResize = true;
+        this.onRotate = false;
         this.setTitle(title);
         this.setSize(w, h);
     }

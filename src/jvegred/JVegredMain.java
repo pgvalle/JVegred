c7908e992e5519883b8c384e26bc3fb1975efd07
pgvalle <pgvalle.nogueira@gmail.com>
2021-03-31 15:04:31 -0300

    restarted everything

--- src/jvegred/JVegredMain.java
+++ src/jvegred/JVegredMain.java
@@ -3,13 +3,7 @@ package jvegred;
 
 public class JVegredMain {
 
-    /**
-     * Entry point for app
-     * 
-     * @param args list of commandline arguments
-     */
     public static void main(String[] args) {
-        JVegredFrame frame = new JVegredFrame("bruh", 500, 500);
-        frame.setVisible(true);
+        System.out.println("From the beginning... :')");
     }
 }

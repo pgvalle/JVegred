b3cf35341480dbc70b9689b083c40e28f5ec34e2
pgvalle <pgvalle.nogueira@gmail.com>
2021-04-05 14:14:01 -0300

    Reseting project

--- src/jvegred/JVegredMain.java
+++
@@ -1,9 +0,0 @@
-// TODO Improve code documentation
-package jvegred;
-
-public class JVegredMain {
-
-    public static void main(String[] args) {
-        System.out.println("From the beginning... :')");
-    }
-}

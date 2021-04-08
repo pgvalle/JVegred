be5371a70c2e3127d982e7820b1db58f64741000
pgvalle <pgvalle.nogueira@gmail.com>
2021-04-07 23:36:45 -0300

    selectionRect, paint, move, resize and rotate selection rect property with method to update it. Move, resize and rotate added intern call to update it. paint removed automatic draw of selection rectangle.

--- src/jvegred/figures/Figure.java
+++ src/jvegred/figures/Figure.java
@@ -1,6 +1,8 @@
 package jvegred.figures;
 
-import java.awt.*;
+import java.awt.Graphics2D;
+import java.awt.Paint;
+import java.awt.geom.Rectangle2D;
 
 public abstract class Figure {
 
@@ -8,6 +10,7 @@ public abstract class Figure {
 
 	public int x, y;
 	public int w, h;
+	public Rectangle2D selectionRect;
 
 	public double angle; // in radians
 	
@@ -16,6 +19,12 @@ public abstract class Figure {
 	protected Figure(int x, int y, Paint fill) {
 		this.x = x; this.y = y;
 		this.w = 50; this.h = 50;
+		this.selectionRect = new Rectangle2D.Double(
+			this.x + Math.cos(this.angle) - 2.0,
+			this.y + Math.sin(this.angle) - 2.0,
+			this.w + Math.cos(this.angle) + 4.0,
+			this.h + Math.sin(this.angle) + 4.0
+		);
 
 		this.active = true;
 
@@ -24,24 +33,33 @@ public abstract class Figure {
 		this.fill = fill;
 	}
 
+	protected void updateSelectionRect() {
+		this.selectionRect.setFrame(
+			this.x + Math.cos(this.angle) - 2.0,
+			this.y + Math.sin(this.angle) - 2.0,
+			this.w + Math.cos(this.angle) + 4.0,
+			this.h + Math.sin(this.angle) + 4.0
+		);
+	}
+
 	public void paint(Graphics2D g2d) {
-		if (this.active) {
-			g2d.setPaint(Color.RED);
-			g2d.drawRect(
-				(int) (this.x + Math.cos(this.angle) - 1.0),
- 				(int) (this.y + Math.sin(this.angle) - 1.0),			
- 				(int) (this.w + Math.cos(this.angle) + 1.0),
- 				(int) (this.h + Math.sin(this.angle) + 1.0)
-			);
-		}
+		// applying rotations
+		g2d.rotate(this.angle, this.x + this.w / 2.0, this.y + this.h / 2.0);
 	}
 
 	public void move(int dx, int dy) {
 		this.x += dx;
 		this.y += dy;
+		this.updateSelectionRect();
 	}
 
 	public void resize(int x1, int y1, int x2, int y2) {
 		// do stuff
+		this.updateSelectionRect();
 	}
+
+	public void rotate(double angle) {
+		this.angle += angle;
+		this.updateSelectionRect();
+	} 
 }

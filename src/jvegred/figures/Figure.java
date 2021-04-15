dba9410916f996982d100fffff071386a42165dc
pgvalle <pgvalle.nogueira@gmail.com>
2021-04-15 18:52:25 -0300

    all fields access change and methods All fields access changed to protected. Removed all methods except move for now

--- src/jvegred/figures/Figure.java
+++ src/jvegred/figures/Figure.java
@@ -2,68 +2,36 @@ package jvegred.figures;
 
 import java.awt.Graphics2D;
 import java.awt.Paint;
-import java.awt.Color;
-import java.awt.Rectangle;
 
 public abstract class Figure {
 
-	public boolean onFocus;
+	protected boolean focused;
 
-	public int x, y;
-	public int w, h;
-	public Rectangle bounds; // selection rect
-
-	public double angle; // in radians
+	protected int x, y;
+	protected int w, h;
 	
-	public Paint fill;
+	protected Paint fill;
 
-	protected Figure(int x, int y, Paint fill) {
-		this.onFocus = true;
+	protected Figure(int x, int y, int w, int h, Paint fill) {
+		this.focused = true;
 
 		this.x = x; this.y = y;
-		this.w = 50; this.h = 50;
-		this.bounds = new Rectangle();
-		this.updateBounds();
-		
-		this.angle = 0.0;
+		this.w = w; this.h = h;
 		
 		this.fill = fill;
 	}
 
-	protected void updateBounds() {
-		int sin = (int) Math.sin(this.angle);
-		int cos = (int) Math.cos(this.angle);
+	public boolean contains(int x, int y) {
+		boolean inx = this.x <= x && x <= this.x + this.w;
+		boolean iny = this.y <= y && y <= this.y + this.h;
 
-		this.bounds.setFrame(
-			this.x - cos - 1, this.y - sin - 1, this.w + cos + 2, this.h - sin + 2
-		);
+		return inx && iny;
 	}
 
-	public void paint(Graphics2D g2d) {
-		if (this.onFocus) {
-			g2d.setColor(Color.RED);
-			g2d.draw(this.bounds);
-		}
-
-		g2d.rotate(this.angle, this.x + this.w / 2.0, this.y + this.h / 2.0);
-	}
+	public abstract void draw(Graphics2D g2d);
 
 	public void move(int dx, int dy) {
 		this.x += dx;
 		this.y += dy;
-
-		this.updateBounds();
 	}
-
-	public void resize(int x1, int y1, int x2, int y2) {
-		// do stuff
-
-		this.updateBounds();
-	}
-
-	public void spin(double angle) {
-		this.angle += angle;
-
-		this.updateBounds();
-	} 
 }

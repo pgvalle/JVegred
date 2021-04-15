e04251acc859843e23ba22af6cb524eacb2ce319
pgvalle <pgvalle.nogueira@gmail.com>
2021-04-15 18:54:37 -0300

    Changed fields access modifiers Now they are protected

--- src/jvegred/figures/Geometric2D.java
+++ src/jvegred/figures/Geometric2D.java
@@ -4,12 +4,11 @@ import java.awt.Paint;
 
 public abstract class Geometric2D extends Figure {
 
-	public Paint outline;
-	
-	public int outlineThickness;
+	protected Paint outline;
+	protected int outlineThickness;
 
-	protected Geometric2D(int x, int y, Paint fill, Paint outline, int outlineThickness) {
-		super(x, y, fill);
+	protected Geometric2D(int x, int y, int w, int h, Paint fill, Paint outline, int outlineThickness) {
+		super(x, y, w, h, fill);
 
 		this.outline = outline;
 		this.outlineThickness = outlineThickness;
